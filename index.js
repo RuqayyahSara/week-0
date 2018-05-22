4910823c2f1134a78e43902b61b4c339c4334247
Ruqayyahsara <sarasadiya4@gmail.com>
2018-05-22 15:52:35 +0530

    week 0 assignment v1

---
+++ index.js
@@ -0,0 +1,27 @@
+var post = {
+
+    status : {
+        type: "string",
+        required : false
+    },
+    askQuestion: {
+        type : "string",
+        required : false
+    },
+    photos : {
+        type: "string",
+        reuired : false
+    },
+    videos : {
+        type : "string",
+        required: false
+    },
+    milestone : {
+        type: "string",
+        required: false
+    },
+   comments: {
+       type: "strings",
+       required : false
+   },
+};
---
+++ js.js
@@ -0,0 +1,12 @@
+var x = 5
+console.log(x);
+
+let y = 5
+console.log(y);
+a = 2*6;
+b = a + 2;
+
+
+
+
+
